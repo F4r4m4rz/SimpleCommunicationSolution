a3e59c1a6c4b1e4cf76ff794c658b53fa0f20130
Faramarz Bodaghi <faramarz.bodaghi@outlook.com>
2020-08-11 22:48:14 +0200

    Aibel_Deeline

---
+++ AibelDeelineInterface/Aibel_Deeline/CORelease.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using AibelDeelineInterface.Common;
+
+namespace AibelDeelineInterface.Aibel_Deeline
+{
+    public class CORelease : Communication
+    {
+        public CORelease() : base()
+        {
+            ControlObjectsList = new List<string>();
+            TargetLocation = LocationEnum.Deeline;
+        }
+
+        [NotMapped]
+        public IEnumerable<string> ControlObjectsList
+        {
+            get
+            {
+                return ControlObjects.Split(';');
+            }
+
+            set
+            {
+                ControlObjects = string.Empty;
+                foreach (var item in value)
+                {
+                    ControlObjects = string.IsNullOrEmpty(ControlObjects) ? $"{item}" : $"{ControlObjects};{item}";
+                }
+            }
+        }
+
+        public string ControlObjects { get; set; }
+
+        public string Module { get; set; }
+
+        public string Area { get; set; }
+
+        public string Section { get; set; }
+
+        public Hold Hold { get; set; }
+    }
+}
---
+++ AibelDeelineInterface/Aibel_Deeline/DrawingRevision.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AibelDeelineInterface.Common;
+
+namespace AibelDeelineInterface.Aibel_Deeline
+{
+    public class DrawingRevision : Communication
+    {
+        public DrawingRevision()
+        {
+            
+        }
+
+        public string Id { get; set; }
+
+    }
+}
